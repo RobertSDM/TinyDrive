f9e53be62d5186314a8eeb8204dfda76b46be0da
Roberto <roberto.menesantos@gmail.com>
2024-03-26 12:05:25 -0300

    feat & refactor: adding styles, file search, ask file in home page. Adding route in main.tsx and adding global configuration in index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./routes/Home.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <body className="w-[100%] h-[100dvh]">
+            <RouterProvider router={router} />
+        </body>
+    </React.StrictMode>
+);
---
+++ src/routes/Home.tsx
@@ -0,0 +1,205 @@
+import { ChangeEvent, useState } from "react";
+import { MdFileUpload, MdExpandLess, MdExpandMore } from "react-icons/md";
+
+enum Extensions {
+    text = ".txt",
+    folder = "-",
+}
+
+function Home() {
+    const [isUploadOpen, setIsUploadOpen] = useState(false);
+    const [search, setSearch] = useState("");
+    const [archives, setArchives] = useState([
+        {
+            name: "Pasta legal",
+            type: "FOLDER",
+        },
+        {
+            name: "Arquivo maneiro",
+            type: "TEXT",
+        },
+    ]);
+
+    const [foundItems, setFoundItems] = useState([] as typeof archives);
+
+    function findArchives(value: string) {
+        const found = archives.filter((i) => {
+            return i.name.toUpperCase().includes(value.toUpperCase());
+        });
+        return setFoundItems(found);
+    }
+
+    const handleFile = (
+        reader: FileReader,
+        event: ChangeEvent<HTMLInputElement>
+    ) => {
+        const fileList = event.target.files!;
+
+        function read(readIndex: number) {
+            if (readIndex >= fileList.length) {
+                return;
+            }
+
+            const file = fileList[readIndex];
+            reader.readAsArrayBuffer(file);
+
+            reader.onload = (e) => {
+                console.log(e);
+                console.log(file);
+                read(++readIndex);
+            };
+        }
+
+        read(0);
+    };
+
+    const handleFolder = (
+        reader: FileReader,
+        event: ChangeEvent<HTMLInputElement>
+    ) => {
+        for (const i of event.target.files!) {
+            console.log(i);
+        }
+    };
+
+    const handleSelection = (isFileInput: boolean) => {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.multiple = true;
+        input.minLength = 1;
+        input.webkitdirectory = !isFileInput;
+
+        const reader = new FileReader();
+        input.addEventListener("change", (e: Event) => {
+            if (isFileInput) {
+                handleFile(
+                    reader,
+                    e as unknown as ChangeEvent<HTMLInputElement>
+                );
+            } else {
+                handleFolder(
+                    reader,
+                    e as unknown as ChangeEvent<HTMLInputElement>
+                );
+            }
+        });
+
+        input.click();
+    };
+
+    return (
+        <>
+            <header className="flex border px-8 py-4 items-center">
+                <h1 className="text-xl font-bold text-purple-500">
+                    Tiny Drive
+                </h1>
+                <div className="mx-auto border-black/40 border rounded-sm  flex items-center gap-x-4 cursor-pointer relative w-2/3">
+                    <input
+                        className="outline-none w-full px-4 py-2"
+                        onChange={(e) => {
+                            findArchives(e.target.value);
+                            setSearch(e.target.value);
+                        }}
+                        value={search}
+                        type="text"
+                        placeholder="Pesquisar no TinyDrive"
+                    />
+                    <div
+                        className={`top-full left-0 border-black/40 border border-t-0 bg-white absolute ${
+                            search.length > 0 ? "flex" : "hidden"
+                        } flex-col w-full`}
+                    >
+                        {foundItems.length > 0 ? (
+                            foundItems.map((i) => (
+                                <span className="hover:bg-purple-100 p-2">
+                                    {i.name}
+                                </span>
+                            ))
+                        ) : (
+                            <span className="text-black/40 text-sm p-2">
+                                Nenhum resultado encontrado
+                            </span>
+                        )}
+                    </div>
+                </div>
+            </header>
+
+            <main className="mt-10 mx-10">
+                <nav className="border-t border-b border-black/10 py-1">
+                    <div
+                        className={`inline relative ${
+                            isUploadOpen ? "border-black/30" : ""
+                        } `}
+                    >
+                        <button
+                            className={`items-center gap-x-2 border-purple-500 text-black border hover:bg-purple-500  hover:text-white p-2 inline-flex cursor-pointer rounded-t-sm`}
+                            onMouseEnter={() => {
+                                setIsUploadOpen(true);
+                            }}
+                            onMouseLeave={() => {
+                                setIsUploadOpen(false);
+                            }}
+                        >
+                            <MdFileUpload />
+                            Carregar
+                            {!isUploadOpen ? (
+                                <MdExpandLess />
+                            ) : (
+                                <MdExpandMore />
+                            )}
+                        </button>
+                        <div
+                            onMouseOver={() => {
+                                setIsUploadOpen(true);
+                            }}
+                            onMouseLeave={() => {
+                                setIsUploadOpen(false);
+                            }}
+                            className={`absolute bg-white w-full border border-black/30 border-t-0 ${
+                                isUploadOpen ? "block" : "hidden"
+                            }`}
+                        >
+                            <button
+                                onClick={() => handleSelection(true)}
+                                className="hover:bg-purple-500 px-2 py-1 hover:text-white cursor-pointer w-full"
+                            >
+                                Arquivos
+                            </button>
+                            <hr className="w-4/5 mx-auto" />
+                            <button
+                                onClick={() => handleSelection(false)}
+                                className="hover:bg-purple-500 px-2 py-1 hover:text-white cursor-pointer w-full"
+                            >
+                                Pastas
+                            </button>
+                        </div>
+                    </div>
+                </nav>
+
+                <section className="mt-5">
+                    <div className="text-xl text-black/50">/</div>
+                    <table className="mt-5 w-full">
+                        <thead>
+                            <tr>
+                                <td className="font-bold w-4/5">Nome</td>
+                                <td className="font-bold">Tipo</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {archives.map((arq) => (
+                                <tr key={arq.name}>
+                                    <td>{arq.name}</td>
+                                    <td className="flex justify-center">
+                                        {Extensions[arq.type.toLowerCase()]}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default Home;
