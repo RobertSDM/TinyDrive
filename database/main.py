9f338d121acc9cb2f2e78f824770f15f591e3fd3
Roberto <roberto.menesantos@gmail.com>
2024-05-01 10:20:41 -0300

    feat & refactor: adding enviroment variables to main.py and changing the prisma file

--- database/main.py
+++ database/main.py
@@ -1,8 +1,18 @@
 from routes import create_app
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 def main():
     app = create_app()
-    app.run(debug=True, port=4500)
+
+    port= os.getenv("PORT") if os.getenv("PORT")  else 4500
+    debug=os.getenv("MODE") != "production" 
+
+    app.run(debug=debug, 
+            port=port)
 
 if __name__ == "__main__":
     main()
+ 
