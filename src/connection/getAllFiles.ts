3160cf04bcd671399d97cd2981f34a845639091c
Roberto <roberto.menesantos@gmail.com>
2024-05-28 11:42:27 -0300

    refactor: chaging backend repository

--- src/connection/getAllFiles.ts
+++ src/connection/getAllFiles.ts
@@ -5,7 +5,7 @@ const getAllFilesByFolderId = async (
     id: string | "/"
 ): Promise<false | IFile[]> => {
     try {
-        const res = await fetch(`${BACKEND_URL}/get_children/${id}`);
+        const res = await fetch(`${BACKEND_URL}get_children/${id}`);
         const resData = await res.json();
 
         if (res.ok) {
@@ -21,7 +21,7 @@ const getAllFilesByFolderId = async (
 
 const getAllRootFiles = async (): Promise<false | Array<IFile & IFolder>> => {
     try {
-        const res = await fetch(`${BACKEND_URL}/get_root_files`);
+        const res = await fetch(`${BACKEND_URL}get_root_files`);
         const resData = await res.json();
 
         if (res.ok) {
