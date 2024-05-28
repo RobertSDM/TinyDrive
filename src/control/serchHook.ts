1ea871ec0189bbbfb1e4790403bcfaadb7c7b171
Roberto <roberto.menesantos@gmail.com>
2024-05-27 21:58:53 -0300

    fix: variable not beeing used commented

--- src/control/serchHook.ts
+++ src/control/serchHook.ts
@@ -1,41 +1,41 @@
-import { MutableRefObject, useRef, useState } from "react";
+// import { MutableRefObject, useRef, useState } from "react";
 
-const useSearchHook = () => {
-    const [search, setSearch] = useState("");
+// const useSearchHook = () => {
+//     const [search, setSearch] = useState("");
 
-    const searchElement = useRef(
-        null
-    ) as MutableRefObject<HTMLInputElement | null>;
+//     const searchElement = useRef(
+//         null
+//     ) as MutableRefObject<HTMLInputElement | null>;
 
-    const [foundItems, setFoundItems] = useState([]);
+//     const [foundItems, setFoundItems] = useState([]);
 
-    // function findArchives(value: string) {
-    //     const found = archives.filter((i) => {
-    //         return i.name.toUpperCase().includes(value.toUpperCase());
-    //     });
-    //     return setFoundItems(found);
-    // }
+//     // function findArchives(value: string) {
+//     //     const found = archives.filter((i) => {
+//     //         return i.name.toUpperCase().includes(value.toUpperCase());
+//     //     });
+//     //     return setFoundItems(found);
+//     // }
 
-    function keyClearSearchInput(e: KeyboardEvent) {
-        if (e.key === "Escape") {
-            setSearch("");
-            searchElement.current!.blur();
-        }
-    }
+//     function keyClearSearchInput(e: KeyboardEvent) {
+//         if (e.key === "Escape") {
+//             setSearch("");
+//             searchElement.current!.blur();
+//         }
+//     }
 
-    function clearSearchInput() {
-        setSearch("");
-    }
+//     function clearSearchInput() {
+//         setSearch("");
+//     }
 
-    return {
-        search,
-        setSearch,
-        // findArchives,
-        foundItems,
-        clearSearchInput,
-        keyClearSearchInput,
-        searchElement,
-    };
-};
+//     return {
+//         search,
+//         setSearch,
+//         // findArchives,
+//         foundItems,
+//         clearSearchInput,
+//         keyClearSearchInput,
+//         searchElement,
+//     };
+// };
 
-export default useSearchHook;
+// export default useSearchHook;
