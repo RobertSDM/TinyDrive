250f525e9469e0d43ab8fbe8795cee6e67496a6d
RobertSDM <roberto.menesantos@gmail.com>
2026-01-09 21:32:14 -0300

    refactor: updating the logic

--- src/features/notification/context/NotifyContext.tsx
+++ src/features/notification/context/NotifyContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useEffect, useState } from "react";
+import { ReactNode, createContext, useEffect, useRef, useState } from "react";
 import { NotificationData } from "../../../types.ts";
 import { Popup } from "../components/Popup.tsx";
 import { Progress } from "../components/Progress.tsx";
@@ -11,7 +11,11 @@ type context = {
 export const NotifyContext = createContext<context>({} as context);
 
 export const Notify = ({ children }: { children: ReactNode }) => {
-    const [notifications, setNotifications] = useState<NotificationData[]>([]);
+    const queue = useRef<NotificationData[]>([]);
+    const isShowing = useRef<boolean>(false);
+
+    const [notificationShown, setNotificationShown] =
+        useState<NotificationData | null>(null);
     const [progress, setProgress] = useState<number>(0);
 
     function done() {
@@ -19,30 +23,45 @@ export const Notify = ({ children }: { children: ReactNode }) => {
     }
 
     function notify(notification: NotificationData) {
-        setNotifications((prev) => [...prev, notification]);
+        if (isShowing.current) {
+            queue.current = [...queue.current, notification];
+        } else {
+            setNotificationShown(notification);
+            isShowing.current = true;
+        }
     }
 
     function dequeue() {
-        const tmp = [...notifications];
-        tmp.shift();
-        setNotifications(tmp);
+        const tmp = [...queue.current];
+        const notificationRemoved = tmp.shift();
+
+        if (!!notificationRemoved) {
+            setNotificationShown(notificationRemoved);
+        } else {
+            setNotificationShown(null);
+            isShowing.current = false;
+        }
+
+        queue.current = tmp;
     }
 
     useEffect(() => {
-        return () => setNotifications([]);
+        return () => {
+            queue.current = [];
+        };
     }, []);
 
     return (
         <NotifyContext.Provider value={{ notify, setProgress }}>
-            {notifications.length > 0 && (
+            {!!notificationShown && (
                 <div className="fixed bottom-0 right-0 z-[60] w-full h-full">
-                    {notifications[0].type === "popup" ? (
-                        <Popup {...{ done, notification: notifications[0] }} />
+                    {notificationShown.type === "popup" ? (
+                        <Popup {...{ done, notification: notificationShown }} />
                     ) : (
                         <Progress
                             {...{
                                 done,
-                                notification: notifications[0],
+                                notification: notificationShown,
                                 progress,
                             }}
                         />
