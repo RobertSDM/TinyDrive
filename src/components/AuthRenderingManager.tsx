58d1dee22dc4f2bb5095781e91406ebaa5027e84
RobertSDM <roberto.menesantos@gmail.com>
2026-02-01 15:41:29 -0300

    fix: changing the Navigate component to useNavigate. The redirect was not working on mobile

--- src/components/AuthRenderingManager.tsx
+++ src/components/AuthRenderingManager.tsx
@@ -1,20 +1,19 @@
 import { useAccountContext as useAccountContext } from "@/context/useContext.tsx";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import LogoLoader from "./LogoLoader.tsx";
 import { useEffect } from "react";
 
 export default () => {
-    const { isLoading, account, refetch } = useAccountContext();
+    const { isLoading, account } = useAccountContext();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if (!account && !isLoading) refetch();
+        if (isLoading) return;
+
+        if (!account) navigate("/login");
     }, [account, isLoading]);
 
     if (isLoading) return <LogoLoader />;
-
-    if (!account) {
-        return <Navigate to="/login" replace />;
-    }
 
     return <Outlet />;
 };
