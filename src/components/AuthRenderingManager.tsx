3a9e6b4996d532aa3b5be0031dc2371024a34cde
RobertSDM <roberto.menesantos@gmail.com>
2026-02-01 11:05:34 -0300

    refactor: adding isLoading to the authRenderingManager rewrite logic, to not rewrite when hot reloading the app

--- src/components/AuthRenderingManager.tsx
+++ src/components/AuthRenderingManager.tsx
@@ -1,12 +1,16 @@
 import { useAccountContext as useAccountContext } from "@/context/useContext.tsx";
 import { Navigate, Outlet } from "react-router-dom";
 import LogoLoader from "./LogoLoader.tsx";
+import { useEffect } from "react";
 
 export default () => {
-    const { isLoading, account } = useAccountContext();
+    const { isLoading, account, refetch } = useAccountContext();
+
+    useEffect(() => {
+        if (!account && !isLoading) refetch();
+    }, [account, isLoading]);
 
     if (isLoading) return <LogoLoader />;
-    
 
     if (!account) {
         return <Navigate to="/login" replace />;
